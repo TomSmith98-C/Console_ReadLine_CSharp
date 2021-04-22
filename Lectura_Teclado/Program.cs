8441cdfaa6e06ecfaa591020121a59d2c5800a29
Tom Smith <74121145+TomSmith98-C@users.noreply.github.com>
2021-04-22 10:23:39 -0500

    Add files via upload

---
+++ Lectura_Teclado/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lectura_Teclado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+
+            Console.WriteLine("Digite el primer numero:  ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo numero:  ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"El resultado de {numero1} + {numero2} es igual a: {numero1+numero2}");
+        }
+    }
+}
